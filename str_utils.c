21047e3496ecc92a1b3350f5ffe287a1cc434b0a
naTan-Sa <natansahlu15@gmail.com>
2026-09-25 23:13:04 +0300

    Add my_strncmp helper

--- str_utils.c
+++ str_utils.c
@@ -56,3 +56,27 @@ void print_str(int fd, const char *str)
 {
 	write(fd, str, my_strlen(str));
 }
+
+/**
+ * my_strncmp - compares two strings up to n characters
+ * @s1: first string
+ * @s2: second string
+ * @n: maximum number of characters to compare
+ *
+ * Return: difference between the first different characters,
+ *         or 0 if the strings match within n characters
+ */
+int my_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+	for(i = 0; i < n; i++)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+
+		if (s1[i] == '\0')
+			return (0);
+	}
+
+	return (0);
+}
