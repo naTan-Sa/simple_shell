cc782ed33cde6b14892cf10bb1746714a4b68590
naTan-Sa <natansahlu15@gmail.com>
2026-09-25 19:16:21 +0300

    Add shell.h with includes and function prototypes

--- shell.h
+++ shell.h
@@ -1,11 +1,25 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 extern char **environ;
 
-void run_command(char *line, char *shell_name);
+/* execute.c */
+void run_command(char **args, char *shell_name);
+
+/* parse.c */
+int count_words(char *str);
 char **split_line(char *line);
-char *my_strdup(const char *str);
+
+/* str_utils.c */
 size_t my_strlen(const char *str);
+char *my_strdup(const char *str);
+void print_str(int fd, const char *str);
 
 #endif
